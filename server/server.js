3064fcbbd5bdfb3a7863d9f35496abda1d5c2845
teodoran <teodoran@stud.ntnu.no>
2015-03-29 20:47:16 +0200

    Added production client settings and fixed ng uglification.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     routes = require('./routes'),
-    db = require('./models');
+    db = require('./models'),
 
-var app = express();
+    app = express(),
+    clientDir = process.env.CLIENT_DIR || process.argv.slice(2)[0] || '/../client';    
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/api', routes);
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + clientDir));
 app.use(express.static(__dirname + '/../bower_components'));
 
 app.listen(app.get('port'), function () {
