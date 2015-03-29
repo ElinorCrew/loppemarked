30bc88860ca25943fc1e9b8923c47c7c175cedbf
teodoran <teodoran@stud.ntnu.no>
2015-03-29 01:47:48 +0100

    Added new gruntfile and fixed parts missed in merge.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ app.use(bodyParser.urlencoded({
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/api', routes);
-app.use(express.static(__dirname + '/../app'));
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../bower_components'));
 
 app.listen(app.get('port'), function () {
     console.log('Loppekartet server is listening on port ' + app.get('port'));
