8d17507ac72487463b203338d5cf3001e63d7d58
teodoran <teodoran@stud.ntnu.no>
2015-04-10 00:04:13 +0200

    Small cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,15 @@
-// Generated on 2015-03-28 using generator-angular 0.11.1
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/**/*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
 
-  // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
-
-  // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
-  var config = {
-      app: 'client',
-      dist: 'build',
-      server: 'server'
-    },
-
-    bowerJson = require('./client/bower.json'),
-    bowerFolder = 'client/bower_components',
-
-    bowerDependencyPaths = function(bowerJson, componentFolder, dependencyTypes) {
-      var paths = [];
-
-      dependencyTypes.forEach(function(type) {
-        for (var dep in bowerJson[type]) {
-          paths.push(componentFolder + '/' + dep + '/' + dep + '.js');
-        }
-      });
-
-      return paths;
-    };
-
-  // Define the configuration for all the tasks
   grunt.initConfig({
 
-    config: config,
-
-    // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
-        files: ['<%= config.app %>/bower.json'],
+        files: ['client/bower.json'],
         tasks: ['wiredep']
       },
       js: {
@@ -64,7 +29,7 @@ module.exports = function (grunt) {
         tasks: ['newer:jshint:spec', 'karma']
       },
       compass: {
-        files: ['<%= config.app %>/styles/**/*.{scss,sass}'],
+        files: ['client/styles/**/*.{scss,sass}'],
         tasks: ['compass', 'autoprefixer:all']
       },
       gruntfile: {
@@ -75,14 +40,13 @@ module.exports = function (grunt) {
           livereload: '<%= connect.options.livereload %>'
         },
         files: [
-          '<%= config.app %>/**/*.html',
-          '<%= config.app %>/styles/**/*.css',
-          '<%= config.app %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}'
+          'client/**/*.html',
+          'client/styles/**/*.css',
+          'client/images/**/*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
     },
 
-    // The actual grunt server settings
     connect: {
       options: {
         port: 9000,
@@ -95,7 +59,7 @@ module.exports = function (grunt) {
           open: true,
           middleware: function (connect) {
             return [
-              connect.static(config.app)
+              connect.static('client')
             ];
           }
         }
@@ -106,7 +70,7 @@ module.exports = function (grunt) {
           middleware: function (connect) {
             return [
               connect.static('spec'),
-              connect.static(config.app)
+              connect.static('client')
             ];
           }
         }
@@ -114,12 +78,11 @@ module.exports = function (grunt) {
       dist: {
         options: {
           open: true,
-          base: '<%= config.dist %>/client'
+          base: 'build/client'
         }
       }
     },
 
-    // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
       options: {
         jshintrc: '.jshintrc',
@@ -129,6 +92,7 @@ module.exports = function (grunt) {
         src: [
           './server/**/*.js',
           './client/**/*.js',
+          'Gruntfile.js',
           '!./client/bower_components/**',
           '!./client/spec/**'
         ]
@@ -140,12 +104,11 @@ module.exports = function (grunt) {
         src: ['./client/spec/**/*.js']
       }
     },
-
-    // Automatically inject Bower components into 
+ 
     wiredep: {
       app: {
-        cwd: config.app,
-        src: ['<%= config.app %>/index.html'],
+        cwd: 'client',
+        src: ['client/index.html'],
         ignorePath:  /\.\.\//
       }
       // Currently not in use.
@@ -153,27 +116,25 @@ module.exports = function (grunt) {
       // <!-- bower:css -->
       // <!-- endbower -->
       // sass: {
-      //   src: ['<%= config.app %>/styles/**/*.{scss,sass}'],
+      //   src: ['client/styles/**/*.{scss,sass}'],
       //   ignorePath: /(\.\.\/){1,2}bower_components\//
       // }
     },
 
-    // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       dist: {
         options: {
-          sassDir: '<%= config.app %>/styles',
-          cssDir: '<%= config.app %>/styles',
-          httpPath: "/",
-          imagesDir: "img",
-          javascriptsDir: "js",
+          sassDir: 'client/styles',
+          cssDir: 'client/styles',
+          httpPath: '/',
+          imagesDir: 'img',
+          javascriptsDir: 'js',
           relativeAssets: true,
           lineComments: false
         }
       }
     },
 
-    // Add vendor prefixed styles
     autoprefixer: {
       options: {
         browsers: ['last 1 version']
@@ -185,13 +146,12 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           flatten: true,
-          src: '<%= config.app %>/styles/*.css',
-          dest: '<%= config.app %>/styles/'
+          src: 'client/styles/*.css',
+          dest: 'client/styles/'
         }]
       }
     },
 
-    // Test settings
     karma: {
       unit: {
         configFile: 'karma.conf.js',
@@ -199,10 +159,9 @@ module.exports = function (grunt) {
       }
     },
 
-    // Empties folders to start fresh
     clean: {
-      dist: [config.dist],
-      css: ['<%= config.app %>/styles/**/*.css']
+      dist: ['build'],
+      css: ['client/styles/**/*.css']
     },
 
     copy: {
@@ -283,21 +242,9 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    // 'wiredep',
-    // 'useminPrepare',
-    // 'concurrent:dist',
-    // 'autoprefixer',
-    // 'concat',
-    // 'ngAnnotate',
     'copy',
-    // 'cdnify',
-    // 'cssmin',
     'uglify'
-    // 'filerev',
-    // 'usemin',
-    // 'htmlmin'
   ]);
-
 
   grunt.registerTask('default', [
     'newer:jshint',
