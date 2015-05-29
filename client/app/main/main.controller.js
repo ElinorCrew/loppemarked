946f19553a5d0003f892fe6463777146eb0cc194
spohner <str3sses@gmail.com>
2015-05-29 23:16:32 +0200

    cleaup to pass jshint

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,11 +4,11 @@ angular.module('loppemarkedApp')
 .controller('MainCtrl', function ($scope, $timeout, leafletData) {
 	$scope.message = 'Hello';
 
-	$scope.$on( "$viewContentLoaded", function(event) {
+	$scope.$on('$viewContentLoaded', function() {
 		leafletData.getMap().then(function(map){
 			$timeout(function() {
 				map.invalidateSize(true);				
-			})
+			});
 		});
 	});
 });
