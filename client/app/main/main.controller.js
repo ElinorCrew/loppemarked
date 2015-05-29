e587225568ff626e87a3723e13aa3236d38171d4
spohner <str3sses@gmail.com>
2015-05-29 23:11:39 +0200

    fixed partial gray map bug

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('loppemarkedApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+.controller('MainCtrl', function ($scope, $timeout, leafletData) {
+	$scope.message = 'Hello';
+
+	$scope.$on( "$viewContentLoaded", function(event) {
+		leafletData.getMap().then(function(map){
+			$timeout(function() {
+				map.invalidateSize(true);				
+			})
+		});
+	});
+});
