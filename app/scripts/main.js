c5e8a7ffcce0feeba7adbf92d5a2480408e5f21f
Steffen <steffen@pohnerhenriksen.com>
2015-11-22 14:41:52 +0100

    changes to load semantic, suboptimals with fonts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,8 @@
-mapboxgl.accessToken = mapBoxToken || process.env.mapBoxToken;
+mapboxgl.accessToken = "pk.eyJ1Ijoic3RlZmZlbnAiLCJhIjoiT25HUl9UbyJ9.zzJv3w5uLNwqcuoh6XUxqQ";
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-    center: [10.725231170654297, 59.91200869359693], // starting position
-    zoom: 12 // starting zoom
-});
-
-$('.ui.dropdown')
-  .dropdown()
-;
+	    container: 'map', // container id
+	    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+	    center: [10.725231170654297, 59.91200869359693], // starting position
+	    zoom: 12 // starting zoom
+	});
+$('.ui.dropdown').dropdown();
