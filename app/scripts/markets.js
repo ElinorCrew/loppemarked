f69882c498e71e8cb4dc6ebe86aa9e6502d1e4f5
Simen Haugerud Granlund <simen@hgranlund.com>
2015-11-22 15:27:01 +0100

    Created inital market model

---
+++ app/scripts/markets.js
@@ -0,0 +1,34 @@
+(function ($) {
+  'use strict';
+
+  var market = function (market) {
+    var self = market;
+    self.save = function () {
+      return self;
+    };
+    self.delete = function () {
+      return self;
+    };
+    self.update = function () {
+      return self;
+    };
+    return self;
+  };
+
+  var Markets = function () {
+    var self = {};
+    var baseUrl = '/api/markets/';
+
+    self.all = function (callback) {
+      $.getJSON(baseUrl, function (data) {
+        var markets = data.map(market);
+        callback(markets);
+      });
+    };
+
+    return self;
+  };
+
+  window.loppe = window.loppe || {};
+  window.loppe.markets = new Markets();
+})($);
