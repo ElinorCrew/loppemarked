def7adc318dde706fe8ca48cb11dbe8c700ec00e
Steffen Pøhner Henriksen <str3sses@gmail.com>
2015-11-23 13:50:28 +0100

    Changed style of map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = "pk.eyJ1Ijoic3RlZmZlbnAiLCJhIjoiY2loYnBxMmtpMHd6M3Vra3RybXZxbjZ2byJ9.NXvjP_UDUfUJZ7_nwhVPzQ";
 var map = new mapboxgl.Map({
 	    container: 'map', // container id
-	    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+	    style: 'mapbox://styles/steffenp/cihby9a4700cjbdm126ge53iq', //stylesheet location
 	    center: [10.725231170654297, 59.91200869359693], // starting position
 	    zoom: 12 // starting zoom
 	});
