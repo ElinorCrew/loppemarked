59c78e23092ff8f7f27f7516fe23e3c8a1da817d
Simen Haugerud Granlund <simen@hgranlund.com>
2015-11-23 23:33:49 +0100

    Initial reactification og the cards

--- app/scripts/component.js
+++ app/scripts/component.js
@@ -1,25 +1,65 @@
 
-var CardExtraContent = React.createClass({
-  displayName: 'CardExtraContent',
+var Cards = React.createClass({
+  displayName: 'Cards',
   render: function() {
+    var markets = this.props.markets;
     return (
-            <div className = "extra content">
-            <span className = "left floated like">
-            <i className = "like icon"> </i>
-            Lik
-            </span>
-            <span className = "right floated star">
-            <i className = "share alternate icon"> </i>
-            Del på facebook
-            </span >
-            </div>
+    <div className="ui list">
+       {markets.map(function(market) {
+           return <Card key={market.id} marketName={market.name}/>;
+        })}
+    </div>
     );
   }
 });
 
+var Card = React.createClass({
+  render: function() {
+    var marketName = this.props.marketName;
+    return (
+      <div className="item">
+        <div className="ui card" style={{width: '100%'}}>
+          <div className="content">
+            <div className="header">{marketName}</div>
+            <div className="meta">
+              <span className="category">Om to dager, kl 18:00</span>
+              <br />
+              <span className="category">Schweigaardsgate 41, 0366 OSLO</span>
+            </div>
+            <div className="description">
+              <p>Loppemarkedet inneholder lopper fra hele Oslo. Det vil bli servert kaffe, boller og brus. Det selges møbler, sykler, antikviteter og ski.</p>
+            </div>
+            <a className="ui yellow left ribbon label">Populær</a>
+          </div>
+        <CardExtraContent/>
+        </div>
+      </div>
+    );
+  }
+});
+
+var CardExtraContent = React.createClass({
+  displayName: 'CardExtraContent',
+  render: function() {
+    return (
+        <div className = "extra content">
+            <span className = "left floated like">
+                <i className = "like icon"> </i>
+                Lik
+            </span>
+            <span className = "right floated star">
+                <i className = "share alternate icon"> </i>
+                Del på facebook
+            </span >
+        </div>
+    );
+  }
+});
 
 $(function() {
+window.loppe.markets.all(function  (markets) {
   ReactDOM.render(
-    React.createElement(CardExtraContent, null),
-    $('#TestReact')[0]);
+                  <Cards markets = {markets}/>,
+                  $('#Cards')[0]);
+    })
 });
