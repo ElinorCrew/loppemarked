eff0e06693d584272614e54febf56ffb63dfe7b9
Simen Haugerud Granlund <simen@hgranlund.com>
2016-02-09 19:55:07 +0100

    Added eventDate to market object

--- server/models/market.js
+++ server/models/market.js
@@ -1,25 +1,29 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   'use strict';
 
-  var Market = sequelize.define('market', {
+  let market = sequelize.define('market', {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     address: DataTypes.STRING,
+    eventDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
     lat: {
       type: DataTypes.DECIMAL(10, 2),
-      get: function() {
-        var lat = this.getDataValue('lat');
+      get: function () {
+        let lat = this.getDataValue('lat');
         return parseFloat(lat);
       }
     },
     lng: {
       type: DataTypes.DECIMAL(10, 2),
-      get: function() {
-        var lat = this.getDataValue('lng');
+      get: function () {
+        let lat = this.getDataValue('lng');
         return parseFloat(lat);
       }
     },
   });
 
-  return Market;
+  return market;
 };
