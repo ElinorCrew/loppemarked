0bd6b66a7667588a44abe1dc8ad5b7f017becbdc
Simen Haugerud Granlund <simen@hgranlund.com>
2016-03-21 21:23:32 +0100

    Only trigger mouseout event when it is not bobled from child elements

--- app/components/LeftMenu.jsx
+++ app/components/LeftMenu.jsx
@@ -1,4 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, {
+  Component, PropTypes
+}
+from 'react';
 import _ from 'underscore';
 import moment from 'moment';
 import MarketCard from 'components/MarketCard';
@@ -12,56 +15,75 @@ import MarketsAction from '../actions/marketActions';
 const cx = classNames.bind(styles);
 
 class LeftMenu extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.marketList = this.marketList.bind(this);
     this.onMouseOut = this.onMouseOut.bind(this);
   }
 
   divideMarkets(markets, divide) {
-     return _.chain(markets)
-             .groupBy(divide) 
-             .mapObject(function (markets) {
-               return _.sortBy(markets, function (market) {return moment(market.eventDate);})
-             })
-             .pairs()
-             .value();
+    return _.chain(markets)
+      .groupBy(divide)
+      .mapObject(function(markets) {
+        return _.sortBy(markets, function(market) {
+          return moment(market.eventDate);
+        })
+      })
+      .pairs()
+      .value();
   }
 
   marketList() {
-    const { select, markets } = this.props;
+    const {
+      select, markets
+    } = this.props;
     var divideMarkets = this.divideMarkets;
-    var yearDividedMarkets = this.divideMarkets(markets, function (market) {return moment(market.eventDate).year();});
-            
+    var yearDividedMarkets = this.divideMarkets(markets, function(market) {
+      return moment(market.eventDate).year();
+    });
+
     return _.chain(yearDividedMarkets)
-            .map(function (yearDivided) {
-              return divideMarkets(yearDivided[1], function (market) {return moment(market.eventDate).month()})
-            })
-            .reduce(function (yearDivider1, yearDivider2) {
-              return yearDivider1.concat(yearDivider2);
-            }, [])
-            .value();
+      .map(function(yearDivided) {
+        return divideMarkets(yearDivided[1], function(market) {
+          return moment(market.eventDate).month()
+        })
+      })
+      .reduce(function(yearDivider1, yearDivider2) {
+        return yearDivider1.concat(yearDivider2);
+      }, [])
+      .value();
   }
 
-  onMouseOut(){
+  onMouseOut() {
+    //We do not want the bobled events.
+    var e = event.toElement || event.relatedTarget;
+    if (e.parentNode == this || e == this) {
+      return;
+    }
     MarketsAction.hover(null);
   }
 
   render() {
-    const {markets } = this.props;
+    const {
+      markets
+    } = this.props;
     const dividedMarkets = this.marketList()
-    return (
-        <div className={cx('four', 'wide', 'column', 'leftmenu')}>
-          <Navigation markets={markets}/>
+    return ( < div className = {
+        cx('four', 'wide', 'column', 'leftmenu')
+      } >
+      < Navigation markets = {
+        markets
+      }
+      />
           <div className='marketList' onMouseOut={this.onMouseOut}>
           {dividedMarkets.map(function (divider) {
-              return <DividedMarketList key={divider} markets={divider[1]} divider={moment.months()[divider[0]]}/>
-            })}
-        </div>
-        </div>
-    );
-  }
+              return <DividedMarketList key={divider} markets={divider[1]} divider={moment.months()[divider[0]]}/ >
+    })
+} < /div>
+        </div >
+);
+}
 }
 
-export default LeftMenu;
-
+export
+default LeftMenu;
